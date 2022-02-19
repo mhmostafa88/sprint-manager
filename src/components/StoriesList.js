6d102f89e3b12d7698585afd6706806ecaac08c0
Mahmoud Mostafa <Mahmoud@mightydigits.com>
2022-02-19 21:11:23 +0200

    fixed most bugs

--- src/components/StoriesList.js
+++ src/components/StoriesList.js
@@ -1,18 +1,18 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { GlobalContext } from '../context/GlobalContext';
-import Calender from './calender';
-import moment from 'moment';
-import { StoriesListContainer, SummaryCard } from './StoriesList.style';
-import Story from './Story';
-import { FaCalendar } from 'react-icons/fa';
-import { StyledButton } from '../App.style';
-import BurnDownChart from './BurnDownChart';
-import { CategoryScale } from 'chart.js';
-import Chart from 'chart.js/auto';
-import StoriesForm from './StoriesForm';
-import { styled } from '@mui/material/styles';
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { GlobalContext } from "../context/GlobalContext";
+import Calender from "./calender";
+import moment from "moment";
+import { StoriesListContainer, SummaryCard } from "./StoriesList.style";
+import Story from "./Story";
+import { FaCalendar } from "react-icons/fa";
+import { StyledButton } from "../App.style";
+import BurnDownChart from "./BurnDownChart";
+import { CategoryScale } from "chart.js";
+import Chart from "chart.js/auto";
+import StoriesForm from "./StoriesForm";
+import { styled } from "@mui/material/styles";
 // import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
-import Tooltip from './tooltip/Tooltip';
+import Tooltip from "./tooltip/Tooltip";
 
 Chart.register(CategoryScale);
 
@@ -21,37 +21,41 @@ const StoriesList = () => {
   const [showSprintPeriodForm, setShowSprintPeriodForm] = useState(false);
   const today = moment();
   const [sprintStartDate, setSprintStartDate] = useState(
-    moment(JSON.parse(localStorage.getItem('sprintStartDate')) || [])
+    moment(JSON.parse(localStorage.getItem("sprintStartDate")) || "")
   );
   const [sprintEndDate, setSprintEndDate] = useState(
-    moment(JSON.parse(localStorage.getItem('sprintEndDate')) || [])
+    moment(JSON.parse(localStorage.getItem("sprintEndDate")) || "")
   );
   const [sprintDaysCount, setSprintDaysCount] = useState(
-    sprintEndDate.diff(sprintStartDate, 'days')
+    sprintEndDate.diff(sprintStartDate, "days")
   );
 
   const completedStoryPoints = stories.reduce(function (acc, curr) {
     return acc + curr.completedPoints;
   }, 0);
 
-  const storyPoints = stories.reduce(function (acc, curr) {
-    return acc + curr.points;
-  }, 0);
+  const storyPoints = useCallback(
+    stories.reduce(function (acc, curr) {
+      return acc + curr.points;
+    }, 0),
+    [stories]
+  );
 
   const toggleShowSprintPeriodForm = () => {
     setShowSprintPeriodForm(!showSprintPeriodForm);
   };
 
   useEffect(() => {
-    setSprintDaysCount(sprintEndDate.diff(sprintStartDate, 'days'));
+    setSprintDaysCount(sprintEndDate.diff(sprintStartDate, "days"));
     // store the sprint start date and the end date
-    localStorage.setItem('sprintStartDate', JSON.stringify(sprintStartDate));
-    localStorage.setItem('sprintEndDate', JSON.stringify(sprintEndDate));
+    localStorage.setItem("sprintStartDate", JSON.stringify(sprintStartDate));
+    localStorage.setItem("sprintEndDate", JSON.stringify(sprintEndDate));
   }, [sprintStartDate, sprintEndDate, storyPoints]);
 
   const getAvgCompleted = () => {
     return (
-      completedStoryPoints / today.diff(sprintStartDate, 'days').toFixed(1)
+      completedStoryPoints /
+      today.diff(sprintStartDate, "days").toFixed(1)
     );
   };
 
@@ -66,11 +70,13 @@ const StoriesList = () => {
   var enumerateDaysBetweenDates = function (startDate, endDate) {
     var dates = [];
 
-    var currDate = moment(startDate).subtract(1,'days').startOf('day');
-    var lastDate = moment(endDate).add(1,'days').startOf('day');
+    var currDate = moment(startDate).startOf("day");
+    var lastDate = moment(endDate).startOf("day");
 
-    while (currDate.add(1, 'days').diff(lastDate) < 0) {
-      dates.push(currDate.clone().format('MM/DD/YY'));
+    currDate.subtract(1,"days")
+
+    while (currDate.add(1, "days").diff(lastDate,"days") < 1) {
+      dates.push(currDate.clone().format("MM/DD/YY"));
     }
 
     return dates;
@@ -88,51 +94,50 @@ const StoriesList = () => {
     return points;
   };
 
-
-
   var enumerateCompletedStoryPointsBetweenDates = function (startDate) {
-    var currDate = moment(startDate).subtract(1,'days')
     
-    var points = [];
 
-    const requiredPointsPerDay = completedStoryPoints/today.diff(currDate,'days').toFixed(1)
+    var points = [];
+    
+    const requiredPointsPerDay =
+      completedStoryPoints / (today.diff(startDate, "days").toFixed(1) - 1);
     var currentPoints = storyPoints;
-    while (currentPoints > 0 && today.diff(currDate, 'days') > 0 )   {
+    var currDate = moment(startDate);
+
+    while (currentPoints > 0 && today.diff(currDate, "days") > 0) {
       points.push(currentPoints);
-      console.log(today.diff(currDate, 'days'),currentPoints,requiredPointsPerDay);
-      currentPoints = (currentPoints - requiredPointsPerDay);
-      console.log(currentPoints)
-      currDate = currDate.add(1, 'days')
+      currentPoints = currentPoints - requiredPointsPerDay;
+      currDate = currDate.add(1, "days");
     }
+    console.log(completedStoryPoints,requiredPointsPerDay, (today.diff(startDate, "days").toFixed(1)))
     return points;
   };
 
   const labels = enumerateDaysBetweenDates(sprintStartDate, sprintEndDate);
-  const datasetBar = enumerateStoryPointsBetweenDates()
-  const datasetLine =  enumerateCompletedStoryPointsBetweenDates(sprintStartDate);
+  const datasetBar = enumerateStoryPointsBetweenDates();
+  const datasetLine =
+    enumerateCompletedStoryPointsBetweenDates(sprintStartDate);
+    
   const data = {
-    // Name of the variables on x-axies for each bar
     labels: labels,
     datasets: [
       {
-        type:'line',
-        label: 'Actual remaining points',
-        borderColor: 'rgb(255, 99, 132)',
-        borderWidth: 2,
-        fill: false,
+        type: "line",
+        label: "Actual remaining points",
+        borderColor: "hsl(250, 56%, 92%)",
+        borderWidth: 1,
+        fill: true,
         data: datasetLine,
       },
       {
-        type: 'bar',
-      label: 'Expected Remaining points',
-      backgroundColor: 'hsl(250, 24%, 48%)',
-      data: datasetBar,
-      borderColor: 'white',
-      borderWidth: 2,
-      }
+        type: "bar",
+        label: "Expected Remaining points",
+        backgroundColor: "hsl(250, 24%, 48%)",
+        data: datasetBar,
+        borderWidth: 2,
+      },
     ],
   };
-
 
   return (
     <StoriesListContainer>
@@ -152,18 +157,18 @@ const StoriesList = () => {
       {showSprintPeriodForm && (
         <div className="CalenderSectionContainer">
           <div className="CalenderContainer">
-            <div>Start Date: {sprintStartDate.format('MM/DD/YY')}</div>
+            <div>Start Date: {sprintStartDate.format("MM/DD/YY")}</div>
             <Calender value={sprintStartDate} onChange={setSprintStartDate} />
           </div>
           <div className="CalenderContainer">
-            <div>End Date: {sprintEndDate.format('MM/DD/YY')}</div>
+            <div>End Date: {sprintEndDate.format("MM/DD/YY")}</div>
             <Calender value={sprintEndDate} onChange={setSprintEndDate} />
           </div>
         </div>
       )}
 
       <div className="sprint-summary">
-        <SummaryCard color={''}>
+        <SummaryCard color={""}>
           <h4>Total Points</h4>
           <span className="card-body">
             {completedStoryPoints > 0
@@ -171,19 +176,19 @@ const StoriesList = () => {
               : storyPoints}
           </span>
         </SummaryCard>
-        <SummaryCard color={getPercentageCompleted() > 80 ? 'green' : 'white'}>
+        <SummaryCard color={getPercentageCompleted() > 80 ? "green" : "white"}>
           <h4>Percentage Completed</h4>
           <span className="card-body">
             {getPercentageCompleted()}
-            {'%'}
+            {"%"}
           </span>
         </SummaryCard>
-        <SummaryCard color={''}>
+        <SummaryCard color={""}>
           <h4>Required Points/day</h4>
           <span className="card-body">{getReqCompleted()}</span>
         </SummaryCard>
         <SummaryCard
-          color={getAvgCompleted() > getReqCompleted() ? 'green' : 'red'}
+          color={getAvgCompleted() > getReqCompleted() ? "green" : "red"}
         >
           <h4>Average Completed Points/day</h4>
           <span className="card-body">{getAvgCompleted()}</span>
